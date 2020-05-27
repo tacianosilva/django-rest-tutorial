35101aae84dc39958680f1eed3feb76696adebf0
Taciano Morais Silva <tacianosilva@gmail.com>
2020-05-27 11:00:58 -0300

    Removendo url do app admin.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from tutorial.quickstart import views
@@ -26,6 +25,5 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
